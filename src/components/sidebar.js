bd9860e8fe934b0f99ccbd25cbfb29de141d6154
Uliah <777uliahshafar@gmail.com>
2020-07-21 06:12:35 +0800

    added sidebaritem

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import {Card, CardBody, CardTitle } from 'reactstrap'
+
 import styles from './_sidebar.module.scss'
 
-const Sidebar = () => (
-    <header className={styles.container}>
-        <div className={styles.sidebar}>
-            Hello World!!
-        </div>
 
-        </header>
+const Sidebar = () => (
+    <container>
+        
+            <Card className={styles.card}>
+                <CardBody>
+                    <CardTitle className="text-center">
+                        <h2>iklan</h2>
+                    </CardTitle>
+                    <img src="https://via.placeholder.com/320x200" alt="Iklan" style={{width:"80%"}}/>
+                </CardBody>
+            </Card>
+            </container>
+
+        
 )
 
 export default Sidebar
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Layout from '../components/layout'
 import Title from '../components/title'
 import ArticleList from  '../components/article-list'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 export default () => (
   <Layout>
     <Title text='Welcome' />
