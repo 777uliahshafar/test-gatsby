86fac89107a2df54baa74e836a9b68549f4b89d9
Uliah <777uliahshafar@gmail.com>
2020-07-25 03:50:06 +0800

    styles burgermenu

--- src/components/Nav/RightNav.js
+++ src/components/Nav/RightNav.js
@@ -1,40 +1,48 @@
 import React from 'react';
+import { Link } from 'gatsby'
 import styled from 'styled-components';
+
+
+const NavLink = props => (
+  <Link className={styled.link} to={props.to}>{props.text}</Link>
+)
 
 const Ul = styled.ul`
   list-style: none;
   display: none; 
   flex-flow: row nowrap;
   li {
-    padding: 18px 10px;
+    padding: 1rem .5rem;
   }
   @media (max-width: 64em) {
     display: flex;
     flex-flow: column nowrap;
-    background-color: #0D2538;
+    background-color: #222f3e;
     position: fixed;
     z-index: 19;
     transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
     top: 0;
     right: 0;
     height: 100vh;
-    width: 300px;
+    width: 18rem;
     padding-top: 3.5rem;
     transition: transform 0.3s ease-in-out;
-    li {
+    a{
       color: #fff;
     }
   }
 `;
 
+
+
 const RightNav = ({ open }) => {
   return (
     <Ul open={open}>
-      <li>Home</li>
-      <li>About Us</li>
-      <li>Contact Us</li>
-      <li>Sign In</li>
-      <li>Sign Up</li>
+      <li><NavLink to='/' text='Articles' /></li>
+      <li><NavLink to='/about' text='About' /></li>
+      <li><NavLink to='/' text='Contact Us' /></li>
+      <li><NavLink to='/' text='Sign In' /></li>
+      <li><NavLink to='/' text='Sign Up' /></li>
     </Ul>
   )
 }
