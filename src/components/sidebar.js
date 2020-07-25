9b1d33e8a701e22e58f106115765e885a1370ab0
Uliah <777uliahshafar@gmail.com>
2020-07-25 12:02:06 +0800

    featured post

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,23 +1,60 @@
 import React from 'react'
-import {Card, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardBody, CardTitle } from 'reactstrap'
+import { StaticQuery, graphql } from 'gatsby'
 
 import styles from './_sidebar.module.scss'
 
+export default () => (
+    <StaticQuery
+        query={graphql`query {
+            allMarkdownRemark(
+                filter: { frontmatter: {featured:{eq: true }}}
+                sort: {fields: [frontmatter___date], order: DESC}
+                ) {
+              totalCount
+              edges{
+                node{
+                    fields{
+                      slug
+                    }
+                    id
+                  frontmatter{
+                    title
+                    image
+                    date(formatString: "MMMM YYYY")
+                  }
+                  excerpt
+                  }
+                }
+              }
+          
+          }`
+        }
 
-const Sidebar = () => (
-    <sidebar className={styles.container}>
-        
-            <Card className={styles.card}>
-                <CardBody >
-                    <CardTitle className="text-center">
-                        <h2>iklan</h2>
-                    </CardTitle>
-                    <img src="https://via.placeholder.com/320x200" alt="Iklan" style={{width:"80%"}}/>
+        render={data => (
+
+            <div className={styles.container}>
+                <Card className={styles.card}>
+                    <CardBody>
+                        <CardTitle className="text-center">Featured Post</CardTitle>
+                {data.allMarkdownRemark.edges.map(({ node }) => (
+                    <a key={node.id} href={node.fields.slug}>{node.frontmatter.title}</a>
+
+                ))}
                 </CardBody>
-            </Card>
-            </sidebar>
+                </Card>
+                
+                <Card className={styles.card}>
+                    <CardBody >
+                        <CardTitle >
+                            <h2>iklan</h2>
+                        </CardTitle>
+                        <img src="https://via.placeholder.com/320x200" alt="Iklan" style={{ width: "80%" }} />
+                    </CardBody>
+                </Card>
 
-        
+
+            </div>//end of div container
+        )}
+    />
 )
-
-export default Sidebar
