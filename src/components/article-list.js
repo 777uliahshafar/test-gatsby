7f5642e9f23db064f92ad8b33f190487935dcfa1
Uliah <777uliahshafar@gmail.com>
2020-07-27 14:44:41 +0800

    modify article, article-list

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -2,16 +2,72 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Article from '../components/article'
 
+const IndexPage = () => (
+  <StaticQuery query={indexQuery} render={data => {
+    return (
+
+      <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Article key={node.id}
+                    path={node.fields.slug}
+                    keyword={node.frontmatter.keyword}
+                    title={node.frontmatter.title}
+                    date={node.frontmatter.date}
+                    excerpt={node.excerpt} />
+            ))}
+        </div>
+
+    )
+  }}/>
+)
+
+const indexQuery = graphql`
+  query {
+    site{
+      siteMetadata{
+        title
+      }
+    }
+
+    allMarkdownRemark {
+      edges {
+        node {
+          fields{
+            slug
+          }
+          id 
+          frontmatter {
+            title
+            image
+            date(formatString: "MMMM YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
+ 
+
+/* Batas 
+
+
+
 export default () => (
     <StaticQuery
-        query = { graphql`query {
+        query = { graphql`query($skip: Int!=0) {
             site {
               siteMetadata{
                 title
               }
             }  
           
-            allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+            allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}
+              limit: 4
+              skip: $skip
+              ) {
               totalCount
               edges{
                 node{
@@ -50,3 +106,5 @@ export default () => (
         )}
     />
 )
+
+*/
--- src/components/article.js
+++ src/components/article.js
@@ -2,6 +2,33 @@ import React from 'react'
 import {Link} from 'gatsby'
 import styles from  './_article.module.scss'
 
+
+const Article = ({ path, keyword, title, date, excerpt }) => {
+   return(
+      <article className={styles.articleBox}>
+         <div className={styles.left}>
+            <img src={'https://source.unsplash.com/150x150/?' + keyword} alt={title} />
+         </div>
+         <div className={styles.right}>
+            <h3>
+               {title}
+            </h3>
+            <div className={styles.date}>{date}
+            </div>
+            <div>
+               {excerpt}
+            </div>
+            <Link to={path}>Read More</Link>
+         </div>
+      </article>
+   )
+}
+
+export default Article
+
+
+/* Batas */
+/*
 export default (props) => (
 
    <Link to={props.to}>
@@ -23,3 +50,5 @@ export default (props) => (
    </Link>
 
 )
+
+*/
