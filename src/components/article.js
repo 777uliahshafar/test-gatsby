655ab6c0493f2aa28fbe7a05808ade9aa5d9dfdc
Uliah <777uliahshafar@gmail.com>
2020-07-28 09:51:58 +0800

    fixed post tags keyid

--- src/components/article.js
+++ src/components/article.js
@@ -1,31 +1,35 @@
 import React from 'react'
-import {Link} from 'gatsby'
-import styles from  './_article.module.scss'
+import { Link } from 'gatsby'
+import styles from './_article.module.scss'
 import Img from 'gatsby-image'
 import { slugify } from '../util/utilityFunction.js'
 
 const Article = ({ path, title, date, excerpt, fluid, tags }) => {
-   return(
+   return (
       <article className={styles.articleBox}>
          <div className={styles.left}>
             <Img fluid={fluid} alt={title} />
          </div>
          <div className={styles.right}>
             <h3>
-             <Link to={path}>{title}</Link>
+               <Link to={path}>{title}</Link>
             </h3>
             <div className={styles.date}>{date}
             </div>
             <div>
                {excerpt}
             </div>
-            <div className={styles.tags}>
-               {tags.map(tag => (
-                  <Link to={`/tag/${slugify(tag)}`}>{tag}</Link> 
-               ))}         
+            <div>
+               <ul className={styles.tags}>
+                  {tags.map(tag => (
+                     <li key={tag}>
+                        <Link to={`/tag/${slugify(tag)}`}>{tag}</Link>
+                     </li>
+                  ))}
+               </ul>
             </div>
             <div className={styles.more}>
-               <Link to={path}>Read More</Link> 
+               <Link to={path}>Read More</Link>
             </div>
          </div>
       </article>
