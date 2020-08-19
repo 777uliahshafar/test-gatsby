475e81f2631f65897ed42151116d8cac47707594
Uliah <777uliahshafar@gmail.com>
2020-08-20 06:23:05 +0800

    comment form v3

--- src/components/comment.js
+++ src/components/comment.js
@@ -7,7 +7,7 @@ const Comments = (props) => {
       <form
         className={s.form}
         method="POST"
-        action="https://komentar-cepa.herokuapp.com/v2/entry/777uliahshafar/gatsbyBlogonline/comment/comments"
+        action="https://komentar-cepa.herokuapp.com/v2/entry/777uliahshafar/gatsbyBlogonline/master/comments"
       >
         <input
           name="fields[slug]"
