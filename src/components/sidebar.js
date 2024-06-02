f33824222a358a0e44a10bc9571c19dcde5531c7
777uliahshafar <777uliahshafar@gmail.com>
2024-06-03 06:24:08 +0800

    sidebrrefactor

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,62 @@
 import React from "react"
 import { Card, CardBody, CardTitle } from "reactstrap"
-import { StaticQuery, graphql } from "gatsby"
-
+import { useStaticQuery, graphql } from "gatsby"
 import { container, card } from "./_sidebar.module.scss"
 
-export default () => (
+export default function Sidebar() {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(
+        filter: { frontmatter: { featured: { eq: true } } }
+        sort: { frontmatter: { date: DESC } }
+      ) {
+        totalCount
+        edges {
+          node {
+            fields {
+              slug
+            }
+            id
+            frontmatter {
+              title
+              date(formatString: "MMMM YYYY")
+            }
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className={container}>
+      <Card className={card}>
+        <CardBody>
+          <CardTitle className="text-center">Featured Post</CardTitle>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <a key={node.id} href={node.fields.slug}>
+              {node.frontmatter.title}
+            </a>
+          ))}
+        </CardBody>
+      </Card>
+      <Card className={card}>
+        <CardBody>
+          <CardTitle>
+            <h2>iklan</h2>
+          </CardTitle>
+          <img
+            src="https://via.placeholder.com/320x200"
+            alt="Iklan"
+            style={{ width: "80%" }}
+          />
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
+
+/* export default () => (
   <StaticQuery
     query={graphql`
       query {
@@ -58,3 +110,4 @@ export default () => (
     )}
   />
 )
+ */
