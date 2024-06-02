d5d9d9413959b0ece51c139fd92ba8a870342d37
777uliahshafar <777uliahshafar@gmail.com>
2024-06-02 14:34:12 +0800

    functionarrow

--- src/components/title.js
+++ src/components/title.js
@@ -1,9 +1,20 @@
 import React from "react"
 import { container, title, subtitle } from "./_title.module.scss"
 
-export default props => (
+const Title = props => {
+  return (
+    <section className={container}>
+      <h1 className={title}>{props.text}</h1>
+      <div className={subtitle}>{props.subtitle}</div>
+    </section>
+  )
+}
+
+export default Title
+
+/* export default props => (
   <section className={container}>
     <h1 className={title}>{props.text}</h1>
     <div className={subtitle}>{props.subtitle}</div>
   </section>
-)
+) */
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,24 @@
 import React from "react"
 import { content, header, errorMessage } from "./_404.module.scss"
 
-export default () => (
+const NotFound = () => {
+  return (
+    <div className={content}>
+      <h1 className={header}>Page not found</h1>
+      <p className={errorMessage}>
+        The page you are looking for does not exists.
+      </p>
+    </div>
+  )
+}
+
+export default NotFound
+
+/* export default () => (
   <div className={content}>
     <h1 className={header}>Page not found</h1>
     <p className={errorMessage}>
       The page you are looking for does not exists.
     </p>
   </div>
-)
+) */
