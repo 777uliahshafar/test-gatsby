bb1b3e76eccb8beaa762e3bdc0cc058c1fbb0c5d
777uliahshafar <777uliahshafar@gmail.com>
2024-06-02 19:57:41 +0800

    ninewarn

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,120 +1,124 @@
-const { createFilePath } = require('gatsby-source-filesystem')
-const path = require('path')
-const { slugify } = require('./src/util/utilityFunction')
-const _ = require('lodash')
+const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
+const { slugify } = require("./src/util/utilityFunction")
+const _ = require("lodash")
+const FilterWarningsPlugin = require("webpack-filter-warnings-plugin")
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-   if (node.internal.type === 'MarkdownRemark') {
-      const { createNodeField } = actions
-      const slug = createFilePath({ node, getNode, basePath: 'markdown' })
-      createNodeField({
-         node,
-         name: 'slug',
-         value: slug,
-      })
-   }
+  if (node.internal.type === "MarkdownRemark") {
+    const { createNodeField } = actions
+    const slug = createFilePath({ node, getNode, basePath: "markdown" })
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
 }
-
 
 // Create post pages programmatically
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
- 
-    return new Promise(resolve => {
-       graphql(`
-       {
-          allMarkdownRemark {
-             edges {
-                node {
-                   fields {
-                      slug
-                   }
-                   frontmatter{
-                      tags
-                   }
-                }
-             }
+  const { createPage } = actions
+
+  return new Promise(resolve => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                tags
+              }
+            }
           }
-       }`
-       ).then(result => {
-          result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-             createPage({
-                path: node.fields.slug,
-                component: path.resolve('./src/templates/post.js'),
-                context: {
-                  slug: node.fields.slug,
-                },
-              })
+        }
+      }
+    `).then(result => {
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve("./src/templates/post.js"),
+          context: {
+            slug: node.fields.slug,
+          },
+        })
 
-            // Create posts pagination pages
-            const articlesPerPage = 4
-            const numberOfPages = Math.ceil(result.data.allMarkdownRemark.edges.length / articlesPerPage)
+        // Create posts pagination pages
+        const articlesPerPage = 4
+        const numberOfPages = Math.ceil(
+          result.data.allMarkdownRemark.edges.length / articlesPerPage
+        )
 
-            Array.from({ length: numberOfPages }).forEach((_, index) => {
-               const isFirstPage = index === 0
-               const currentPage = index + 1
+        Array.from({ length: numberOfPages }).forEach((_, index) => {
+          const isFirstPage = index === 0
+          const currentPage = index + 1
 
-               if (isFirstPage) return
+          if (isFirstPage) return
 
-               createPage({
-                  path: `/page/${currentPage}`,
-                  component: path.resolve('./src/templates/articles-list.js'),
-                  context: {
-                    limit: articlesPerPage,
-                    skip: index * articlesPerPage,
-                    numberOfPages: numberOfPages,
-                    currentPage: currentPage,
-                  },
-                })
-            })
-               
-
-
-            
-            //Get all Tags
-             let tags = []
-             _.each(result.data.allMarkdownRemark.edges, edge => {
-                if (_.get(edge, 'node.frontmatter.tags')) {
-                   tags = tags.concat(edge.node.frontmatter.tags)
-                }
-
-             })
-
-             let tagPostCounts = {}
-             tags.forEach(tag => {
-                tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1;
-             })
-
-             tags = _.uniq(tags)
-            //Create tags page
-             createPage({
-               path: `/tags`,
-               component: path.resolve('./src/templates/post-tags.js'),
-               context: {
-                  tags,
-                  tagPostCounts,
-                },
-             })
-            
-             //end of tags page
-
-             //Create tag posts
-            tags.forEach(tag => {
-               createPage({
-                  path: `/tag/${slugify(tag)}`,
-                  component: path.resolve('./src/templates/tag-posts.js'),
-                  context: {
-                     tag,
-                   },
-                })
-            })
-            //end of tag post
-
-
+          createPage({
+            path: `/page/${currentPage}`,
+            component: path.resolve("./src/templates/articles-list.js"),
+            context: {
+              limit: articlesPerPage,
+              skip: index * articlesPerPage,
+              numberOfPages: numberOfPages,
+              currentPage: currentPage,
+            },
           })
-          resolve()
-       })
+        })
 
+        //Get all Tags
+        let tags = []
+        _.each(result.data.allMarkdownRemark.edges, edge => {
+          if (_.get(edge, "node.frontmatter.tags")) {
+            tags = tags.concat(edge.node.frontmatter.tags)
+          }
+        })
 
+        let tagPostCounts = {}
+        tags.forEach(tag => {
+          tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+        })
+
+        tags = _.uniq(tags)
+        //Create tags page
+        createPage({
+          path: `/tags`,
+          component: path.resolve("./src/templates/post-tags.js"),
+          context: {
+            tags,
+            tagPostCounts,
+          },
+        })
+
+        //end of tags page
+
+        //Create tag posts
+        tags.forEach(tag => {
+          createPage({
+            path: `/tag/${slugify(tag)}`,
+            component: path.resolve("./src/templates/tag-posts.js"),
+            context: {
+              tag,
+            },
+          })
+        })
+        //end of tag post
+      })
+      resolve()
     })
- }
+  })
+}
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new FilterWarningsPlugin({
+        exclude: /mini-css-extract-plugin[^]*Conflicting order. Following module has been added:/,
+      }),
+    ],
+  })
+}
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,12 @@ import React from "react"
 /* import { container, footer } from "./_footer.module.scss"
  */
 import * as styles from "./_footer.module.scss"
-export default ({ children }) => (
-  <footer className={styles.container}>
-    <div className={styles.footer}>{children}</div>
-  </footer>
-)
+
+const Footer = ({ children }) => {
+  return (
+    <footer className={styles.container}>
+      <div className={styles.footer}>{children}</div>
+    </footer>
+  )
+}
+export default Footer
