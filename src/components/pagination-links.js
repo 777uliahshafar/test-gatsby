387768adbb8f50a0d12d0c644800a88196cfdb05
777uliahshafar <777uliahshafar@gmail.com>
2024-06-06 17:11:58 +0800

    archivepagedone

--- src/components/pagination-links.js
+++ src/components/pagination-links.js
@@ -1,48 +1,25 @@
 import React from "react"
-import { Pagination, PaginationItem, PaginationLink } from "reactstrap"
+import { Link } from "gatsby"
 
-const PaginationLinks = ({ currentPage, numberOfPages }) => {
+const PaginationLinks = ({ currentPage, numPages }) => {
   const isFirst = currentPage === 1
-  const isLast = currentPage === numberOfPages
-  const previousPage =
-    currentPage - 1 === 1 ? "/" : "/page/" + (currentPage - 1).toString()
-  const nextPage = "/page/" + (currentPage + 1).toString()
+  const isLast = currentPage === numPages
+  const prevPage =
+    currentPage - 1 === 1 ? "/archive" : `/archive/${currentPage - 1}`
+  const nextPage = `/archive/${currentPage + 1}`
   return (
-    <Pagination aria-label="Page navigation example">
-      {isFirst ? (
-        <PaginationItem disabled>
-          <PaginationLink previous href="/" />
-        </PaginationItem>
-      ) : (
-        <PaginationItem>
-          <PaginationLink previous href={previousPage} />
-        </PaginationItem>
+    <div>
+      {!isFirst && (
+        <div style={{ float: "left" }}>
+          <Link to={prevPage}>&#8592; Previous Page</Link>
+        </div>
       )}
-      {Array.from({ length: numberOfPages }, (_, i) =>
-        currentPage === i + 1 ? (
-          <PaginationItem active key={`page-number${i + 1}`}>
-            <PaginationLink href={`/${i === 0 ? "" : "page/" + (i + 1)}`}>
-              {i + 1}
-            </PaginationLink>
-          </PaginationItem>
-        ) : (
-          <PaginationItem key={`page-number${i + 1}`}>
-            <PaginationLink href={`/${i === 0 ? "" : "page/" + (i + 1)}`}>
-              {i + 1}
-            </PaginationLink>
-          </PaginationItem>
-        )
+      {!isLast && (
+        <div style={{ float: "right" }}>
+          <Link to={nextPage}>Next Page &#8594;</Link>
+        </div>
       )}
-      {isLast ? (
-        <PaginationItem disabled>
-          <PaginationLink next href={nextPage} />
-        </PaginationItem>
-      ) : (
-        <PaginationItem>
-          <PaginationLink next href={nextPage} />
-        </PaginationItem>
-      )}
-    </Pagination>
+    </div>
   )
 }
 
--- src/templates/articles-list.js
+++ src/templates/articles-list.js
@@ -3,25 +3,16 @@ import Layout from "../components/layout"
 import Title from "../components/title"
 import Article from "../components/article"
 import PaginationLinks from "../components/pagination-links.js"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 
-const articlesList = ({ pageContext, data }) => {
-  const { edges } = data.allMarkdownRemark
-
-  const { currentPage, numPages } = pageContext
-  const isFirst = currentPage === 1
-  const isLast = currentPage === numPages
-  const prevPage =
-    currentPage - 1 === 1 ? "/archive" : `/archive/${currentPage - 1}`
-  const nextPage = `/archive/${currentPage + 1}`
-
-  /*   const articles = props.data.allMarkdownRemark.edges
-  const { currentPage } = props.pageContext */
+const articlesList = props => {
+  const articles = props.data.allMarkdownRemark.edges
+  const { currentPage, numPages } = props.pageContext
 
   return (
     <Layout>
       <Title text={`Page: ${currentPage}`}></Title>
-      {edges.map(({ node }) => (
+      {articles.map(({ node }) => (
         <Article
           key={node.id}
           slug={node.fields.slug}
@@ -33,16 +24,7 @@ const articlesList = ({ pageContext, data }) => {
           tags={node.frontmatter.tags}
         />
       ))}
-      {!isFirst && (
-        <div style={{ float: "left" }}>
-          <Link to={prevPage}>&#8592; Previous Page</Link>
-        </div>
-      )}
-      {!isLast && (
-        <div style={{ float: "right" }}>
-          <Link to={nextPage}>Next Page &#8594;</Link>
-        </div>
-      )}
+      <PaginationLinks currentPage={currentPage} numPages={numPages} />
     </Layout>
   )
 }
