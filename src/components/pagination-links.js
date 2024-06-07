4ad0f52737e4aa4590f8f884b1b7d3e1c7a81462
777uliahshafar <777uliahshafar@gmail.com>
2024-06-07 17:53:06 +0800

    articleslistcss

--- src/components/pagination-links.js
+++ src/components/pagination-links.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import * as styles from "./_pagination-links.module.scss"
 
 const PaginationLinks = ({ currentPage, numPages }) => {
   const isFirst = currentPage === 1
@@ -8,15 +9,19 @@ const PaginationLinks = ({ currentPage, numPages }) => {
     currentPage - 1 === 1 ? "/archive" : `/archive/${currentPage - 1}`
   const nextPage = `/archive/${currentPage + 1}`
   return (
-    <div>
+    <div className={styles.container}>
       {!isFirst && (
-        <div style={{ float: "left" }}>
-          <Link to={prevPage}>&#8592; Previous Page</Link>
+        <div className={styles.left}>
+          <div className={styles.prev}>
+            <Link to={prevPage}>&#8592; Previous Page</Link>
+          </div>
         </div>
       )}
       {!isLast && (
-        <div style={{ float: "right" }}>
-          <Link to={nextPage}>Next Page &#8594;</Link>
+        <div className={styles.right}>
+          <div className={styles.next}>
+            <Link to={nextPage}>Next Page &#8594;</Link>
+          </div>
         </div>
       )}
     </div>
